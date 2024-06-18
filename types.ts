91a0b425a21d394ff3ed8a0e1bf5b6eaecf57b0d
xu zhang <shinhwagk@outlook.com>
2024-06-18 13:42:01 +0800

    Update types.ts

--- types.ts
+++ types.ts
@@ -16,14 +16,4 @@ export interface ResponseData {
   mts?: number
 }
 
-export type DBStore = 'file' | 'memory'
-
-export enum HJDBErrorMsg {
-  HJDB001 = "table not exist.",
-  HJDB002 = "database not exists.",
-}
-
-export enum HJDBErrorCode {
-  HJDB001 = "HJDB-001",
-  HJDB002 = "HJDB-002",
-}
+export type DBStore = 'file' | 'memory';
